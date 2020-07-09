d28fe52319bf31fa65329075d58b6a4332287b8a
unknown <godls036@gmail.com>
2020-07-09 16:59:27 +0900

    Set DEBUG to False

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
-print(DEBUG)
+DEBUG = False
+
 ALLOWED_HOSTS = [
     "chocomusic-dev.eba-mrdqgmau.ap-northeast-2.elasticbeanstalk.com",
 ]
