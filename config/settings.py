d0ec5e0661c468e598efb769362011335ea76fcf
unknown <godls036@gmail.com>
2020-07-10 13:18:18 +0900

    set mode debug to test

--- config/settings.py
+++ config/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
