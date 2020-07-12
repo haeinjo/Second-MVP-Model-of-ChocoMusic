4a3add8af70c989a26a7d1ac0f313b0b99cad2ca
unknown <godls036@gmail.com>
2020-07-12 21:26:39 +0900

    swap database to postgresql

--- config/settings.py
+++ config/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
