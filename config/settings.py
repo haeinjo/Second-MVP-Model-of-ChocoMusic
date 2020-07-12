82b2d1008f6ca9c9a45b08e28149623bef862284
unknown <godls036@gmail.com>
2020-07-12 13:16:01 +0900

    clean requirements.txt and packages.config

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".elasticbeanstalk.com",
+    "127.0.0.1",
 ]
 
 
